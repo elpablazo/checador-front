9757f65e363ae8383e04576c0823dc979f8b1b56
Pablo <pablo.vazquezg@protonmail.com>
2023-07-27 03:01:02 -0600

    Crear componente de dropdown Fixes #10

---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,39 @@
+<script>
+	import ChevronDown from '$components/icons/ChevronDown.svelte';
+	import { cubicIn, cubicInOut, cubicOut, quintOut } from 'svelte/easing';
+	import { fade, scale, slide } from 'svelte/transition';
+
+	export let disabled = false;
+
+	export let isOpen = false;
+	export let toggle = () => {
+		if (disabled) return;
+		isOpen = !isOpen;
+	};
+</script>
+
+<div class="relative inline-block text-left">
+	<button
+		class={`inline-flex w-full disabled:bg-gray-100 disabled:text-gray-400 justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${
+			disabled && 'cursor-not-allowed'
+		}`}
+		on:click={toggle}
+	>
+		Seleccionar <ChevronDown className="-mr-1  h-5 w-5 text-gray-400" />
+	</button>
+
+	{#if isOpen}
+		<div
+			transition:scale={{ duration: 150, opacity: 0, start: 0.9, easing: quintOut }}
+			class={`absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+			role="menu"
+			aria-orientation="vertical"
+			aria-labelledby="menu-button"
+			tabindex="-1"
+		>
+			<div class="py-1" role="none">
+				<slot />
+			</div>
+		</div>
+	{/if}
+</div>
