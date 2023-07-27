d576a5c959a9eb2c9a24d67d04ae5c19652db5ed
Pablo <pablo.vazquezg@protonmail.com>
2023-07-27 14:50:01 -0600

    Error 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Planet from '$components/icons/Planet.svelte';
+</script>
+
+{#if $page.status === 404}
+	<div
+		class="container flex flex-col items-center justify-center h-full mx-auto font-semibold text-gray-800"
+	>
+		<div class="flex items-center">
+			<span class="text-8xl">4</span>
+			<Planet className="w-24 h-24 animate-pulse ease-in" />
+			<span class="text-8xl">4</span>
+		</div>
+		<p class="text-xl text-center">Página no encontrada.</p>
+	</div>
+{/if}
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import Button from '$components/Button.svelte';
-	import Input from '$components/Input.svelte';
-	import Link from '$components/Link.svelte';
-</script>
