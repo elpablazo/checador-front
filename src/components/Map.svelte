eae889ab2f3ae4852ac28776405879ea8d8217a7
Pablo <pablo.vazquezg@protonmail.com>
2023-07-27 23:32:23 -0600

    no se alch

---
+++ src/components/Map.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	const lang = browser ? navigator.language : null;
+
+	let coordinates: any = null;
+
+	onMount(async () => {
+		const leaflet = await import('leaflet');
+
+		navigator.geolocation.getCurrentPosition((position) => {
+			coordinates = position.coords;
+			console.log(position);
+		});
+		if (browser) {
+		}
+	});
+</script>
+
+<p class="w-full text-center">El lenguaje del navegador es: {lang}</p>
