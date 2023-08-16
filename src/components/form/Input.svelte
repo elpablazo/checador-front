6a19c69a8ab2472710d89b5fe97cf163ad8c7415
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-08-15 20:16:19 -0600

    Se usa svelte-forms-lib para validación de campos

--- src/components/form/Input.svelte
+++ src/components/form/Input.svelte
@@ -1,6 +1,7 @@
 <!-- // Todo: agregar cn para classnames (todos los componentes) -->
 <!-- Todo: agregar todos los input types restantes -->
 <script lang="ts">
+	import { Field, ErrorMessage } from 'svelte-forms-lib';
 	export let type:
 		| 'text'
 		| 'password'
@@ -19,22 +20,26 @@
 	export let placeholder: string = '';
 	export let value: string = '';
 	export let disabled: boolean = false;
+	// Todo: que cuando haya un error, la clase cambie a border-red-500
+	export let error: boolean | string = false;
 
-	const classname =
-		'block w-full px-4 py-2 text-gray-700 border-0 transition-all rounded-md enabled:shadow-sm ring-1 ring-inset ring-gray-300 outline-0 ring-offset-0 placeholder:text-gray-400 disabled:bg-gray-100 enabled:focus:ring-black ring-offset-0';
+	$: classname =
+		'block w-full px-4 py-2 text-gray-700 border-0 transition-all rounded-md border-1 border-gray-300 enabled:shadow-sm outline-0 ring-offset-0 placeholder:text-gray-500 bg-gray-50 disabled:bg-gray-100 ring-offset-0' +
+		(error ? ' border-red-500' : '');
 </script>
 
 <div class="flex flex-col gap-1">
-	<label for={name} class="font-medium">{label}</label>
+	<label for={name} class="text-gray-800 text-sm">{label}</label>
 	{#if type === 'text'}
-		<input type="text" {name} {placeholder} {disabled} bind:value class={classname} />
+		<Field type="text" {name} {placeholder} {disabled} bind:value class={classname} />
 	{:else if type === 'password'}
-		<input type="password" {name} {placeholder} {disabled} bind:value class={classname} />
+		<Field type="password" {name} {placeholder} {disabled} bind:value class={classname} />
 	{:else if type === 'number'}
-		<input type="number" {name} {placeholder} {disabled} bind:value class={classname} />
+		<Field type="number" {name} {placeholder} {disabled} bind:value class={classname} />
 	{:else if type === 'email'}
-		<input type="email" {name} {placeholder} {disabled} bind:value class={classname} />
+		<Field type="email" {name} {placeholder} {disabled} bind:value class={classname} />
 	{:else if type === 'tel'}
-		<input type="tel" {name} {placeholder} {disabled} bind:value class={classname} />
+		<Field type="tel" {name} {placeholder} {disabled} bind:value class={classname} />
 	{/if}
+	<ErrorMessage {name} class="text-red-500 text-xs" />
 </div>
