422a689996edbb9e7b2545c2111af3e732331a4e
Pablo <52553480+elpablazo@users.noreply.github.com>
2023-08-21 18:51:39 -0600

    Conectar con next auth Fixes #19

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,36 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Credentials from '@auth/core/providers/credentials';
-import axios from 'axios';
 import { payloadApi } from '$lib/payloadApi';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle = SvelteKitAuth({
-	providers: [],
+	providers: [
+		Credentials({
+			credentials: {
+				email: { label: 'Email', type: 'email' },
+				password: { label: 'Password', type: 'password' }
+			},
+			authorize: async ({ email, password }) => {
+				try {
+					// Enviamos los datos de login al servidor y obtenemos al usuario
+					const { data } = await payloadApi.post('/workers/login', { email, password });
+
+					return {
+						id: data?.user?.id,
+						email: data?.user?.email,
+						image: data?.user?.profilePicture?.url || null,
+						name: data?.user?.name,
+						token: data?.token,
+						expires: data?.exp
+					};
+				} catch (error: any) {
+					return null;
+				}
+			}
+		})
+	],
 	pages: {
-		signIn: '/login'
+		signIn: '/login',
+		error: '/login'
 	}
-});
+}) satisfies Handle;
